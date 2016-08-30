06f6008f474daf5ddd2334c8d021ecf44d0628d1
Andy Eccleston <eandy5000@yahoo.com>
2016-08-29 22:08:36 -0500

    titleCase basics work

--- textUtilites.js
+++ textUtilites.js
@@ -8,9 +8,15 @@ var expect = require('chai').expect;
 // practice sexpect(true).to.be.true;
 
 function titleCase (title){
-    return title;
+    return title.split(' ')
+                .map(function(word){
+                    return word[0].toUpperCase() + word.substring(1);
+                })
+                .join(' ');
 };
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo')).to.equal('Vertigo');
 
 expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
